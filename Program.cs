e8ab5d2345147a12520217edf62d1a9817bbac62
Anton Garri Fagerbakk <garrifagerbakk@hotmail.com>
2019-01-25 22:03:39 +0100

    pushing almost finnished project, need to do testing and some other stuff

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+
+namespace internassignment
+{
+    public class OldShipException : Exception
+    {
+        public OldShipException(int year)
+        : base(String.Format("The vessels year {0} is too old!", year))
+        {
+
+        }
+    }
+
+    public class Vessel
+    {
+        private string name;
+        private string yearBuilt;
+
+        public Vessel(string Name, string Year)
+        {
+            Name = name;
+            Year = yearBuilt;
+        }
+
+        public string GetName
+        {
+            get { return name; }
+            set{ name = value; }
+        }
+
+        public string GetYearBuilt
+        {
+            get { return yearBuilt; }
+            set{ yearBuilt = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Vessel : {name}, {yearBuilt}";
+        }
+
+
+        class Ferry : Vessel{
+            private int passengers;
+
+            public Ferry(string Name, string Year, int Passengers) : base(Name, Year) => this.Passengers = Passengers;
+
+            public int Passengers { get => passengers; set => passengers = value; }
+
+            public override string ToString(){
+                return $"Vessel : {name}, {yearBuilt}, {passengers}";
+
+            }
+        }
+        class Tugboat : Vessel{
+            private int maxForce;
+
+            public Tugboat(string Name, string Year) : base(Name, Year)
+            {
+            }
+
+            public int MaxForce { get => maxForce; set => maxForce = value; }
+
+            public override string ToString(){
+                return $"Vessel : {name}, {yearBuilt}, {maxForce}";
+
+            }
+        }
+
+        class Submarine : Vessel{
+            private int maxDepth;
+
+            public Submarine(string Name, string Year) : base(Name, Year)
+            {
+            }
+
+            public int MaxDepth { get => maxDepth; set => maxDepth = value; }
+            
+            public override string ToString(){
+                return $"Vessel : {name}, {yearBuilt}, {maxDepth}";
+
+            }
+        }
+
+        void GetVesselInfo(string vessel){
+            ToString();
+        }
+
+    static void Main(string[] args)
+    {
+        while(true){
+            try
+            {
+                int currentYear = DateTime.Now.Year;
+                
+                Console.Write("Enter vessel name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter year built: ");
+                string yearBuilt = Console.ReadLine();
+                Console.Write("Additional info: ");
+                int addInfo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(name + yearBuilt + addInfo + currentYear);
+                    
+            if(string.IsNullOrEmpty(name)){
+                throw new InvalidOperationException("Name is null or blank");
+            }
+
+            if((currentYear - int.Parse(yearBuilt)) > 20 ){
+                throw new OldShipException(int.Parse(yearBuilt));
+            }
+                Vessel myShip = new Ferry(name, yearBuilt, addInfo);
+            }
+            catch (OldShipException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+    }
+
+}
+}    
+     
+
---
+++ doc/questions.md
@@ -0,0 +1,8 @@
+# Task 1
+
+*Q1*: What is the purpose of the private fields name and yearBuilt having methods for getting their values?
+The purpose for private fields is to able to set different values or information for objects like yearBuilt and name without changing the information in the class Vessel. By using "private" we encapsulate the information and we do not overwrite variables. So when we use the getter methods, we will not have the wrong information returned.
+
+
+*Q2*:  What is the concept behind overriding the ToString() method called?
+The concept behind overriding ToString() is to format and control what the output of the information. This way we can control the output for the viewer.
